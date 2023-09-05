3fb6efb808075a82005082925f2ecda937de3eaa
Jose Manuel Vera <josemanuelvera1991@gmail.com>
2023-09-05 23:58:06 +0200

    Create login and logout functions and use on HomePage login and header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,26 +3,59 @@ import exitLogout from "../../assets/exitLogout.png";
 import Navigation from "../Navigation/Navigation";
 import "./Header.css";
 import Button from "../Button/Button";
+import auth from "../../firebase/firebase";
+import { signOut } from "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 const Header = (): React.ReactElement => {
-  return (
-    <header className="header">
-      <div className="header__container">
-        <div className="header__container-logo">
-          <img
-            className="header__logo"
-            src={galaxyPadelLogo}
-            alt="Galaxy Padel logo app"
-          />
-          <h1 className="header__title">Galaxy Padel</h1>
-        </div>
-        <Button className="header__logout">
-          <img className="header__logout" src={exitLogout} alt="Exit icon" />
-        </Button>
-      </div>
-      <Navigation />
-    </header>
-  );
-};
+  const [user] = useAuthState(auth);
 
+  const logout = async () => {
+    await signOut(auth);
+  };
+
+  if (user) {
+    return (
+      <>
+        <header className="header">
+          <div className="header__container">
+            <div className="header__container-logo">
+              <img
+                className="header__logo"
+                src={galaxyPadelLogo}
+                alt="Galaxy Padel logo app"
+              />
+              <h1 className="header__title">Galaxy Padel</h1>
+            </div>
+            <Button className="header__logout" onClick={logout}>
+              <img
+                className="header__logout"
+                src={exitLogout}
+                alt="Exit icon"
+              />
+            </Button>
+          </div>
+          <Navigation />
+        </header>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <header className="header">
+          <div className="header__container">
+            <div className="header__container-logo">
+              <img
+                className="header__logo"
+                src={galaxyPadelLogo}
+                alt="Galaxy Padel logo app"
+              />
+              <h1 className="header__title">Galaxy Padel</h1>
+            </div>
+          </div>
+        </header>
+      </>
+    );
+  }
+};
 export default Header;
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,15 +1,28 @@
 import Button from "../../components/Button/Button";
+import { signInWithPopup } from "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
 import imageLogin from "../../assets/imageLogin.png";
 import "./HomePage.css";
+import auth, { githubProvider } from "../../firebase/firebase";
 
 const HomePage = (): React.ReactElement => {
+  const [user] = useAuthState(auth);
+
+  const login = async () => {
+    await signInWithPopup(auth, githubProvider);
+  };
+
   return (
     <section className="login-container">
       <h2 className="login-text">Login to access your account</h2>
-      <Button className="login-button">
-        <img src={imageLogin} alt="Github logo" />
-        Login via Github
-      </Button>
+      {user ? (
+        <span className="login-user">{`Welcome ${user?.displayName}`}</span>
+      ) : (
+        <Button className="login-button" onClick={login}>
+          <img src={imageLogin} alt="Github logo" />
+          Login via Github
+        </Button>
+      )}
     </section>
   );
 };
