5c11f12de702adc658d8b98c24acaa5b8d2b49ab
Jose Manuel Vera <josemanuelvera1991@gmail.com>
2023-09-09 14:29:19 +0200

    Test RacketsPage list and restructure Rackets page

--- src/components/RacketsList/RacketsList.tsx
+++ src/components/RacketsList/RacketsList.tsx
@@ -1,13 +1,5 @@
-import "./RacketsList.css";
-
 const RacketsList = (): React.ReactElement => {
-  return (
-    <>
-      <div className="list">
-        <h2 className="list-title">Padel Professional Rackets</h2>
-      </div>
-    </>
-  );
+  return <></>;
 };
 
 export default RacketsList;
--- src/pages/RacketsPage/RacketsPage.tsx
+++ src/pages/RacketsPage/RacketsPage.tsx
@@ -1,10 +1,12 @@
 import RacketsList from "../../components/RacketsList/RacketsList";
+import "./RacketsPage.css";
 
 const RacketsPage = (): React.ReactElement => {
   return (
-    <>
+    <div className="list-page">
+      <h2 className="list-page__title">Padel Professional Rackets</h2>
       <RacketsList />
-    </>
+    </div>
   );
 };
 
