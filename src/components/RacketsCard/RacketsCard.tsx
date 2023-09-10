2e88c435ad70f0891bb2318faff84aa7ec4968ed
Jose Manuel Vera <josemanuelvera1991@gmail.com>
2023-09-10 12:27:46 +0200

    Test RacketsCard component

--- src/components/RacketsCard/RacketsCard.tsx
+++ src/components/RacketsCard/RacketsCard.tsx
@@ -11,13 +11,17 @@ const RacketCard = ({
 }: RacketsCardProps): React.ReactElement => {
   return (
     <article className="racket">
-      <img className="racket__image" src={`${image}`} alt={`${image}`} />
+      <img className="racket__image" src={`${image}`} alt={`${name} racket`} />
       <h2 className="racket__name">{name}</h2>
 
       <div className="racket__container">
         <ul className="racket__atributes">
-          <li>{shape}</li>
-          <li>{weight}g</li>
+          <li>
+            <span aria-label="shape">{shape}</span>
+          </li>
+          <li>
+            <span aria-label="weight">{weight}g</span>
+          </li>
         </ul>
         <Button className="button button__fill">See details</Button>
       </div>
