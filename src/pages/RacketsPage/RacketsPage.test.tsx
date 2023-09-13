67aec0206713ffdab540304c72097c7b1a293660
Jose Manuel Vera <josemanuelvera1991@gmail.com>
2023-09-13 21:18:22 +0200

    Create NoRackets component and test it

--- src/pages/RacketsPage/RacketsPage.test.tsx
+++ src/pages/RacketsPage/RacketsPage.test.tsx
@@ -1,20 +1,36 @@
 import { render, screen } from "@testing-library/react";
 import RacketsPage from "./RacketsPage";
 import { Provider } from "react-redux";
-import { store } from "../../store";
+import { setupStore } from "../../store";
+import { Auth } from "firebase/auth";
+import { racketsMock } from "../../mocks/racketsMock";
+import React from "react";
+
+vi.mock("react", async () => {
+  const actual: Auth = await vi.importActual("react");
+  return {
+    ...actual,
+    useEffect: vi.fn(),
+  };
+});
 
 describe("Given a RacketsList component", () => {
   describe("When it's rendered", () => {
-    test("Then it should show a heading 'Padel Professional Rackets'", () => {
+    test("Then it should show a heading 'Padel Professional Rackets'", async () => {
       const textTitle = "Padel Professional Rackets";
+      const store = setupStore({ racketsState: { rackets: racketsMock } });
 
       render(
         <Provider store={store}>
-          <RacketsPage />
+          <React.Suspense>
+            <RacketsPage />
+          </React.Suspense>
         </Provider>,
       );
 
-      const headingTitle = screen.getByRole("heading", { name: textTitle });
+      const headingTitle = await screen.findByRole("heading", {
+        name: textTitle,
+      });
 
       expect(headingTitle).toBeInTheDocument();
     });
