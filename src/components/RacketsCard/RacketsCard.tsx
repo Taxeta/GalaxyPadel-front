e86963b6e7201b02335a597c54a1a56a23c0f02e
Jose Manuel Vera <josemanuelvera1991@gmail.com>
2023-09-14 11:13:38 +0200

    Add delete icon and dispatch delete action

--- src/components/RacketsCard/RacketsCard.tsx
+++ src/components/RacketsCard/RacketsCard.tsx
@@ -1,6 +1,9 @@
 import { Racket } from "../../types";
 import Button from "../Button/Button";
+import deleteIcon from "../../assets/deleteIcon.svg";
 import "./RacketsCard.css";
+import { useAppDispatch } from "../../store";
+import { deleteRacketActionCreator } from "../../store/rackets/racketsSlice";
 
 interface RacketsCardProps {
   racket: Partial<Racket>;
@@ -8,11 +11,21 @@ interface RacketsCardProps {
 }
 
 const RacketCard = ({
-  racket: { name, image, shape, weight },
+  racket: { id, name, image, shape, weight },
   racketPosition,
 }: RacketsCardProps): React.ReactElement => {
+  const dispatch = useAppDispatch();
+
+  const deleteRacket = () => {
+    dispatch(deleteRacketActionCreator(id!));
+  };
   return (
     <article className="racket">
+      <div className="button-container">
+        <Button className="button-icon" onClick={deleteRacket}>
+          <img className="delete-icon" src={deleteIcon} alt="Delete icon" />
+        </Button>
+      </div>
       <img
         className="racket__image"
         src={image}
@@ -20,7 +33,6 @@ const RacketCard = ({
         loading={racketPosition > 2 ? "lazy" : "eager"}
       />
       <h2 className="racket__name">{name}</h2>
-
       <div className="racket__container">
         <ul className="racket__atributes">
           <li>{shape}</li>
