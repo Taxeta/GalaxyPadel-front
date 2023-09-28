5d7149b2d9ba5c9471bc683aca361ce64e17c8f8
Jose Manuel Vera <josemanuelvera1991@gmail.com>
2023-09-28 23:49:57 +0200

    Fix css specificity to switch Mui component

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,10 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./components/App/App";
-import "./styles/styles.css";
 import { store } from "./store";
 import "@fontsource/nunito";
 import "@fontsource/assistant";
+import "./styles/styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
